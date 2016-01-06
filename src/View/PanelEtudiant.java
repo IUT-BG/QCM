4574a921fb1805c694b716f914c7bb55edeea54a
totolouis <louismagand@gmail.com>
2016-01-06 14:32:32 +0100

    Scoll fonctionne, mis en page 50% pour les questions & réponses du qcm

--- src/View/PanelEtudiant.java
+++ src/View/PanelEtudiant.java
@@ -10,6 +10,9 @@ import Model.Qcm;
 import Model.Question;
 import Model.Reponse;
 import Test.TestQcm;
+import java.awt.Dimension;
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
 import java.util.ArrayList;
 import javax.swing.BorderFactory;
 import javax.swing.JButton;
@@ -26,18 +29,22 @@ import javax.swing.border.TitledBorder;
 public class PanelEtudiant extends JPanel {
 
     //maj QCM eleve
+    JPanel affiche_qcm;
     Qcm qcm;
     Etudiant etu;
     TestQcm test;
     ArrayList<JRadioButton> liste_radio;
     
     public PanelEtudiant() {
-        TitledBorder bord = BorderFactory.createTitledBorder("Qcm");
-        this.setBorder(bord);
+        affiche_qcm = new JPanel();
         
-        JScrollPane jsp = new JScrollPane(JScrollPane.VERTICAL_SCROLLBAR_AS_NEEDED, 
-                JScrollPane.HORIZONTAL_SCROLLBAR_NEVER);
-        this.add(jsp);
+        TitledBorder bord = BorderFactory.createTitledBorder("Qcm");
+        
+        JScrollPane jsp = new JScrollPane(JScrollPane.VERTICAL_SCROLLBAR_ALWAYS, 
+                JScrollPane.HORIZONTAL_SCROLLBAR_ALWAYS); 
+        jsp.setBorder(bord);
+        
+        jsp.setPreferredSize(new Dimension(600,400));
         
         test = new TestQcm();
 
@@ -47,130 +54,48 @@ public class PanelEtudiant extends JPanel {
         liste_radio = new ArrayList();
 
         affQcm();
+        
+        //jsp.add(affiche_qcm);
+        jsp.setViewportView(affiche_qcm);
+        this.add(jsp);
     }
 
     public void affQcm() {
         int i = 0;
-
+        
+        affiche_qcm.setLayout(new GridBagLayout());
+        GridBagConstraints c = new GridBagConstraints();
+        c.gridx = 0;
+        c.gridy = 0;
+        
         for (Question q : etu.getQcm().getQ()) {
+            c.anchor = GridBagConstraints.WEST;
+            c.gridwidth = 10;
             JLabel label_q = new JLabel(q.getIntitule());
-            this.add(label_q);
+            affiche_qcm.add(label_q,c);
+            c.gridy++;
             for (Reponse r : q.getReponse()) {
+                c.anchor = GridBagConstraints.EAST;
+                c.gridwidth = 1;
                 JLabel label_r = new JLabel(r.getIntitule());
-                this.add(label_r);
+                affiche_qcm.add(label_r,c);
+                
+                c.anchor = GridBagConstraints.WEST;
+                c.ipadx = 20;
+                c.gridx++;
                 JRadioButton bt_r = new JRadioButton();
                 liste_radio.add(bt_r);
-                this.add(bt_r);
+                affiche_qcm.add(bt_r,c);
+                c.gridx++;
+                c.ipadx = 0;
             }
-            
+            c.gridy = c.gridy + 1;
+            c.gridx = 0;
             i++;
         }
-         for (Question q : etu.getQcm().getQ()) {
-            JLabel label_q = new JLabel(q.getIntitule());
-            this.add(label_q);
-            for (Reponse r : q.getReponse()) {
-                JLabel label_r = new JLabel(r.getIntitule());
-                this.add(label_r);
-                JRadioButton bt_r = new JRadioButton();
-                liste_radio.add(bt_r);
-                this.add(bt_r);
-            }
-            
-            i++;
-        }
-          for (Question q : etu.getQcm().getQ()) {
-            JLabel label_q = new JLabel(q.getIntitule());
-            this.add(label_q);
-            for (Reponse r : q.getReponse()) {
-                JLabel label_r = new JLabel(r.getIntitule());
-                this.add(label_r);
-                JRadioButton bt_r = new JRadioButton();
-                liste_radio.add(bt_r);
-                this.add(bt_r);
-            }
-            
-            i++;
-        }
-           for (Question q : etu.getQcm().getQ()) {
-            JLabel label_q = new JLabel(q.getIntitule());
-            this.add(label_q);
-            for (Reponse r : q.getReponse()) {
-                JLabel label_r = new JLabel(r.getIntitule());
-                this.add(label_r);
-                JRadioButton bt_r = new JRadioButton();
-                liste_radio.add(bt_r);
-                this.add(bt_r);
-            }
-            
-            i++;
-        }
-            for (Question q : etu.getQcm().getQ()) {
-            JLabel label_q = new JLabel(q.getIntitule());
-            this.add(label_q);
-            for (Reponse r : q.getReponse()) {
-                JLabel label_r = new JLabel(r.getIntitule());
-                this.add(label_r);
-                JRadioButton bt_r = new JRadioButton();
-                liste_radio.add(bt_r);
-                this.add(bt_r);
-            }
-            
-            i++;
-        }
-             for (Question q : etu.getQcm().getQ()) {
-            JLabel label_q = new JLabel(q.getIntitule());
-            this.add(label_q);
-            for (Reponse r : q.getReponse()) {
-                JLabel label_r = new JLabel(r.getIntitule());
-                this.add(label_r);
-                JRadioButton bt_r = new JRadioButton();
-                liste_radio.add(bt_r);
-                this.add(bt_r);
-            }
-            
-            i++;
-        }
-              for (Question q : etu.getQcm().getQ()) {
-            JLabel label_q = new JLabel(q.getIntitule());
-            this.add(label_q);
-            for (Reponse r : q.getReponse()) {
-                JLabel label_r = new JLabel(r.getIntitule());
-                this.add(label_r);
-                JRadioButton bt_r = new JRadioButton();
-                liste_radio.add(bt_r);
-                this.add(bt_r);
-            }
-            
-            i++;
-        }
-               for (Question q : etu.getQcm().getQ()) {
-            JLabel label_q = new JLabel(q.getIntitule());
-            this.add(label_q);
-            for (Reponse r : q.getReponse()) {
-                JLabel label_r = new JLabel(r.getIntitule());
-                this.add(label_r);
-                JRadioButton bt_r = new JRadioButton();
-                liste_radio.add(bt_r);
-                this.add(bt_r);
-            }
-            
-            i++;
-        }
-                for (Question q : etu.getQcm().getQ()) {
-            JLabel label_q = new JLabel(q.getIntitule());
-            this.add(label_q);
-            for (Reponse r : q.getReponse()) {
-                JLabel label_r = new JLabel(r.getIntitule());
-                this.add(label_r);
-                JRadioButton bt_r = new JRadioButton();
-                liste_radio.add(bt_r);
-                this.add(bt_r);
-            }
-            
-            i++;
-        }
-
+        c.gridy = i*2;
+        c.gridx = 0;
         JButton bt_valid = new JButton("Valider");
-        this.add(bt_valid);
+        affiche_qcm.add(bt_valid,c);
     }
 }
